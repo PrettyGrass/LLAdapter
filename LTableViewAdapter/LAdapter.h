7df607f42575c6aa647c523ff60406c86843a600
ylin <ylin8890@gmail.com>
2018-01-01 17:45:15 +0800

    整理

--- LTableViewAdapter/LAdapter.h
+++ LTableViewAdapter/LAdapter.h
@@ -10,16 +10,19 @@
 #define LTableViewAdapter_h
 
 #import "LTabbleViewDefine.h"
+
 #import "LCollectionViewAdapter.h"
 #import "LTableViewAdapter.h"
 #import "LTableViewEditAdapter.h"
-#import "UICollectionViewCell+LModel.h"
-#import "UITableViewCell+LModel.h"
+
 #import "LTableSection.h"
 #import "LCollectSection.h"
-#import "UICollectionViewCell+LModel.h"
-#import "UITableViewCell+LModel.h"
+
 #import "LTableCell.h"
 #import "LCollectCell.h"
+
+#import "UITableViewCell+LModel.h"
+#import "UICollectionViewCell+LModel.h"
+
 
 #endif /* LTableViewAdapter_h */
